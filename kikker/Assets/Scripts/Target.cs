a0ccff0ad9fea5fcc112d7aa27504626f4aed56e
DouweHeijmerink <douweheijmerink@gmail.com>
2019-03-30 00:08:58 +0100

    intial commit

---
+++ kikker/Assets/Scripts/Target.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Target : MonoBehaviour
+{
+    public float health = 50f;
+    public GameObject completeLevelUI;
+
+    public void TakeDamage (float amount)
+    {
+
+        //Finishes the level whenever a target's health get below zero.
+        health -= amount;
+        if (health <= 0f)
+        {
+            CompleteLevel();
+        }
+    }
+
+    //Plays the end level animation, the actual transition to the new scene happens at the end of the animation.
+    public void CompleteLevel()
+    {
+        completeLevelUI.SetActive(true);
+    }
+}
